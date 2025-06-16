c2c0275d382669c22477fd1c8aecae0b89601842
Need4Speed15 <codybrown_151@outlook.com>
2025-06-16 15:06:48 -0400

    initial commit

---
+++ resume.js
@@ -0,0 +1,10 @@
+<div class="sub-section">
+        <strong>Driver</strong> | Amazon, Oklahoma City, OK | June 2021 – June 2022
+        <ul>
+          <li>Conducted over 1,500 hours of flight instruction to students as authorized by FAA regulations</li>
+          <li>Maintained detailed records and monitored the progress of 145 students</li>
+          <li>Supported flight operations with additional duties like aircraft fueling and maintenance communication</li>
+          <li>Achieved a 95% student pass rate on first-time checkrides</li>
+          <li>Organized and led 40 safety workshops and seminars covering weather hazards, aircraft maintenance issues, and in-flight emergency management</li>
+        </ul>
+      </div>
